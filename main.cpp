cd8612a37168cf7b4e2377eb8112fc67da1c8478
andys1987 <salvaticoandres@gmail.com>
2021-08-28 20:45:30 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Se muestra un mensaje por pantalla.
+    cout << "Hola Mundo" << " Para Argentina Programa." << "\n";
+
+    return 0;
+}
